ac49c3f227a9f2f079fb6ef554d1e5c738888cf1
pratik <pasinhal@gmail.com>
2021-01-20 20:49:42 +0530

    changes

--- src/main/java/com/pratik/application/service/ServiceController.java
+++ src/main/java/com/pratik/application/service/ServiceController.java
@@ -1,14 +1,45 @@
 package com.pratik.application.service;
 
+import org.apache.logging.log4j.spi.LoggerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping(value = "/api")
 public class ServiceController {
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String get() {
+        Logger logger = LoggerFactory.getLogger(ServiceController.class);
         return "GET API";
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
